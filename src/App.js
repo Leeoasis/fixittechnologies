3d83c9c25c8454f1f667aaced2da9da6ad201782
Leslie <Kooliesk@gmail.com>
2024-09-04 18:00:49 +0200

    Create basic file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./routes/Home";
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-600"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="App">
+            <h1 className="text-5xl text-emerald-800">Home Page</h1>
+        </div>
+    );
+}
+
+export default HomePage;
