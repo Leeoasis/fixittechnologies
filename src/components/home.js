12806cce5c8d99c127f42280fc4cc11b247d58a0
Leslie <Kooliesk@gmail.com>
2024-09-05 15:58:55 +0200

    create the home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,21 @@
 import React from "react";
+import HeroSection from "./homeComponents/Hero";
+import Promotions from "./homeComponents/Promotions";
+import FeaturedProducts from "./homeComponents/FeaturedProducts";
+import WhyChooseUs from "./homeComponents/WhyChooseUs";
+import Testimonials from "./homeComponents/Testimonials";
+import NewsletterSignup from "./homeComponents/Newsletter";
 
 const HomePage = () => {
     return (
-        <div className="App">
-            <h1 className="text-5xl text-emerald-800">Home Page</h1>
-        </div>
+       <div>
+        <HeroSection/>
+        <Promotions/>
+        <FeaturedProducts/>
+        <WhyChooseUs/>
+        <Testimonials/>
+        <NewsletterSignup/>
+      </div>
     );
 }
 
